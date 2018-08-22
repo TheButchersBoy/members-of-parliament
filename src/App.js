be885d75dce4d62b8ebfb40d96c8ad4d0447ce28
Shadman190319141 <41980481+Shadman190319141@users.noreply.github.com>
2018-08-22 17:01:46 +1000

    Add files via upload
    New CSS classes added in assets folder. The MP person is moved towards the center. A footer is added to the App.js class so that its available across the entire content.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React, { Component } from 'react';
-import SingleMP from './pages/singleMP/SingleMP';
-
+import React, { Component } from "react";
+import SingleMP from "./pages/singleMP/SingleMP";
+import "./assets/stylesheets/footer.css";
 class App extends Component {
   render() {
     return (
       <div>
         <SingleMP />
+        <div className="footer">
+          <p>
+            Content Created By @Nathan,Angelica Shadman for Advanced Internet
+            Programming
+          </p>
+        </div>
       </div>
     );
   }
--- src/pages/singleMP/SingleMP.js
+++ src/pages/singleMP/SingleMP.js
@@ -1,13 +1,28 @@
-import React, { Component } from 'react';
-import temp from '../../assets/temp.png';
+import React, { Component } from "react";
+import temp from "../../assets/temp.png";
+
+import "../../assets/stylesheets/MpImages.css";
 
 class SingleMP extends Component {
   render() {
     return (
       <div>
-        <img src={temp} style={{width: 100}} />
-        <h2>John Smith</h2>
-        <p>Labour party</p>
+        <div className="SizeTitle">
+          <img src={temp} className="CircularImage" />
+          <div className="MpHeadings">
+            <h2>John Smith</h2>
+            <p>Labour party</p>
+          </div>
+        </div>
+        <div className="MpValues">
+          <table>
+            <tr>
+              <th>Issues he cares about</th>
+              <th>Voted Yes</th>
+              <th>Voted No</th>
+            </tr>
+          </table>
+        </div>
       </div>
     );
   }
