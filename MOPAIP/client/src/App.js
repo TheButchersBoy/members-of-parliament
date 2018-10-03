0977e8aa818fc4e6f546c126b66aa2efc9ba4446
Jel <angelicatriant@gmail.com>
2018-10-03 16:20:16 +1000

    Renamed auth foder and routes

--- MOPAIP/client/src/App.js
+++ MOPAIP/client/src/App.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import Navbar from './components/layout/Navbar';
-import Footer from './components/layout/Footer';
-import Landing from './components/layout/Landing';
-import Register from './components/auth/Register';
-import Login from './components/auth/Login';
+import Navbar from "./components/shared/Navbar";
+import Footer from "./components/shared/Footer";
+import Landing from "./components/shared/Landing";
+import Register from "./components/pages/Register";
+import Login from "./components/pages/Login";
 
-import './App.css';
+import "./App.css";
 
 class App extends Component {
   render() {
