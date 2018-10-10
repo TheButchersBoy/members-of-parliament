dc43d7a764c5f68e49b84de440dbb0e15f8883e2
Shadman190319141 <41980481+Shadman190319141@users.noreply.github.com>
2018-10-10 18:46:30 +1100

    Uploaded new Dashboard

--- MOPAIP2018/clients/src/components/dashboard/Dashboard.js
+++ MOPAIP2018/clients/src/components/dashboard/Dashboard.js
@@ -8,6 +8,85 @@ import { getCurrentProfile } from "../../actions/profileActions";
 import Spinner from "../common/Spinner";
 import Button from "./Button";
 class Dashboard extends Component {
+  componentDidMount() {
+    this.props.getCurrentProfile();
+  }
+
+  render() {
+    const { user } = this.props.auth;
+    const { profile, loading } = this.props.profile;
+
+    let dashboardContent;
+    console.log(profile);
+
+    if (profile == null || profile.preference == null || loading) {
+      dashboardContent = <Spinner />;
+      dashboardContent = (
+        <div>
+          <p className="lead text-muted">Welcome {user.name}</p>
+          <p className="lead text-muted">To get started : </p>
+
+          <p className="lead list">
+            {" "}
+            Go to the discussion forum to discuss about issues that you care
+            about
+          </p>
+
+          <p className="lead">
+            Head over to issues page to see current issues that are being
+            discussed and being voted on.
+          </p>
+          <p className="lead">Head over to MP page to see your leaders</p>
+        </div>
+      );
+    } else {
+      //Check if logged in user has profile data
+      if (Object.keys(profile).length > 0) {
+        dashboardContent = <h4>TO DO DISPLAY PROFILE</h4>;
+      }
+    }
+
+    return (
+      <div className="dashboard">
+        <div className="container">
+          <div className="col-md-12">
+            <h1 className="display-4">Dashboard</h1>
+            {dashboardContent}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+const mapStsteToProps = state => ({
+  profile: state.profile,
+  auth: state.auth
+});
+Dashboard.PropTypes = {
+  getCurrentProfile: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired
+};
+const mapStateToProps = state => ({
+  profile: state.profile,
+  auth: state.auth
+});
+
+export default connect(
+  mapStateToProps,
+  { getCurrentProfile }
+)(Dashboard);
+/*
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+
+import { connect } from "react-redux";
+import "./dashboard.css";
+import { getCurrentProfile } from "../../actions/profileActions";
+import Spinner from "../common/Spinner";
+import Button from "./Button";
+class Dashboard extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -50,7 +129,7 @@ class Dashboard extends Component {
   }
   */
 
-    /*
+/*
     const getAbortionPreferences = this.setState.getAbortionPreferences;
     const getHIRPreferences = this.setState.getHIRPreferences;
     const getGayPreferences = this.setState.getGayPreferences;
@@ -83,7 +162,7 @@ class Dashboard extends Component {
         preferenceArray: state.preferenceArray.push(current)
       }));
     }
-    */
+    
     var color = this.state.black;
     let current = e.target.getAttribute("dataTest");
     console.log(current);
@@ -203,3 +282,4 @@ export default connect(
   mapStsteToProps,
   { getCurrentProfile }
 )(Dashboard);
+*/
