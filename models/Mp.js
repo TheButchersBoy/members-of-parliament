9830e58a2bb381566edde75407a04856c4776334
Jel <angelicatriant@gmail.com>
2018-10-15 06:06:06 +1100

    Updated Mp Schema

--- models/Mp.js
+++ models/Mp.js
@@ -1,12 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 // Create Schema
 const MpSchema = new Schema({
+  positionID: {
+    type: Number,
+    required: true
+  },
   firstName: {
     type: String,
     required: true
-  },  
+  },
   lastName: {
     type: String,
     required: true
@@ -14,7 +18,15 @@ const MpSchema = new Schema({
   party: {
     type: String,
     required: true
+  },
+  division: {
+    type: String,
+    required: true
+  },
+  url: {
+    type: String,
+    required: true
   }
 });
 
-module.exports = Mp = mongoose.model('mp', MpSchema);
+module.exports = Mp = mongoose.model("mp", MpSchema);
