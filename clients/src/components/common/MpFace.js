8ec6db5e6da2a7eeb5ac7c05640dc1d0a73137f2
Jel <angelicatriant@gmail.com>
2018-10-15 19:36:22 +1100

    Fixed compiling errors

--- clients/src/components/common/MpFace.js
+++ clients/src/components/common/MpFace.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-const mpFaces = require.context('../../img/reps', true);
+const mpFaces = require.context("../../img/repPhotos", true);
 
-export default (props) => {
+export default props => {
   return (
-  	<div className="row">
-			<div className="col-md-4 offset-md-4">
-    		<img className="mp-face img-fluid" src={mpFaces(`./${props.id}.png`)} />
-			</div>
+    <div className="row">
+      <div className="col-md-4 offset-md-4">
+        <img className="mp-face img-fluid" src={mpFaces(`./${props.id}.png`)} />
+      </div>
     </div>
   );
 };
--- clients/src/components/common/MpInfo.js
+++ clients/src/components/common/MpInfo.js
@@ -1,22 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const mpFaces = require.context('../../img/reps', true);
+const mpFaces = require.context("../../img/repPhotos", true);
 
-export default (props) => {
+export default props => {
   return (
-  	<div className="mp-info-container">
-  		<Link to={props.link}>
-        <img className="mp-face clickable-area img-fluid" src={mpFaces(`./${props.info._id}.png`)} />
-	    </Link>
-  		<div className='text-center'>
-  			<div>
-	  			<span>{props.info.firstName} {props.info.lastName}</span>
-  			</div>
-  			<div>
-	  			<span>{props.info.party}</span>
-  			</div>
-  		</div>
+    <div className="mp-info-container">
+      <Link to={props.link}>
+        <img
+          className="mp-face clickable-area img-fluid"
+          src={mpFaces(`./${props.info._id}.png`)}
+        />
+      </Link>
+      <div className="text-center">
+        <div>
+          <span>
+            {props.info.firstName} {props.info.lastName}
+          </span>
+        </div>
+        <div>
+          <span>{props.info.party}</span>
+        </div>
+      </div>
     </div>
   );
 };
